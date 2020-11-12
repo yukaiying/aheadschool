68feea91dbc211945780a1d32d3cdd6cd37ac399
GB <3105979073@qq.com>
2020-11-12 18:02:08 +0800

    4

--- src/main/java/com/ahead/school/servlet/StudentTestServlet.java
+++ src/main/java/com/ahead/school/servlet/StudentTestServlet.java
@@ -1,20 +1,13 @@
 package com.ahead.school.servlet;
 
-import com.ahead.school.dao.StudentDao;
-import com.ahead.school.entity.Student;
 import com.ahead.school.dao.StudentTestDao;
-
-import com.ahead.school.entity.StudentTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
 
 @Service
 public class StudentTestServlet {
 
     @Autowired
-    private StudentTestDao studentTetsDao;
+    private StudentTestDao studentTestDao;
 
 }
